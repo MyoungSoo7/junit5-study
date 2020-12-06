fdf5b84843a4358fa9643aec14b07441d91be4a6
doyoung0205 <doyoung0205@naver.com>
2020-12-06 15:08:15 +0900

    Initial commit

---
+++ src/test/java/me/doyoung/junit5study/Junit5StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.doyoung.junit5study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
